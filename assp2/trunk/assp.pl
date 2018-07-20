3e29aab09d4e36df96f1f1ab297739332149dd0d
thockar <thockar@0fe50de6-8848-4dc9-b7b8-2b2e66d1568f>
2018-07-20 10:29:27 +0000

    version 2.6.2 build (18201) ***W2K8R2-FIX KB4338818,KB4339093,KB4340556*** requires at least Perl 5.10 best run is on Perl 5.26.x for all platforms supports Perl 5.10 to 5.28 code signature implemented

--- assp2/trunk/assp.pl
+++ assp2/trunk/assp.pl
@@ -587,7 +587,7 @@ our %NotifyFreqTF:shared = (     # one notification per timeframe in seconds per
     'error'   => 60
 );
 
-sub __cs { $codeSignature = '7C396B0C3E2EA18E12266D95EA24A5E20E310316'; }
+sub __cs { $codeSignature = 'CA4F5FCE6D824C294BBD4DCCD2EA219EBBBE7E9E'; }
 
 #######################################################
 # any custom code changes should end here !!!!        #
@@ -68435,7 +68435,7 @@ sub downASSP {
     mlog(0,'initializing shutdown sequence');
     mlogWrite();
     $WorkerName = 'Shutdown';
-    if ($isNoWIN || ($isWIN && Win32::GetOSName =~ /$downASSPFixRe/i)) {$sequenceOK &&= &closeAllSMTPListeners;}   # w2k8r2 KB4338818,KB4339093,KB4340556
+    if ($isNoWIN || ($isWIN && Win32::GetOSName !~ /$downASSPFixRe/i)) {$sequenceOK &&= &closeAllSMTPListeners;}   # w2k8r2 KB4338818,KB4339093,KB4340556
     mlogWrite();
     $sequenceOK &&= &stopSMTPThreads;
     mlogWrite();
@@ -68456,7 +68456,7 @@ sub downASSP {
     mlogWrite();
     &clearDBCon();
     mlogWrite();
-    if ($isNoWIN || ($isWIN && Win32::GetOSName =~ /$downASSPFixRe/i)) {&closeAllWEBListeners;}   # w2k8r2 KB4338818,KB4339093,KB4340556
+    if ($isNoWIN || ($isWIN && Win32::GetOSName !~ /$downASSPFixRe/i)) {&closeAllWEBListeners;}   # w2k8r2 KB4338818,KB4339093,KB4340556
     mlogWrite();
     &syncWriteConfig() if $enableCFGShare;
     &removeLeftCrashFile();
